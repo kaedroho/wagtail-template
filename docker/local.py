0a364e8dd7845efb407e2a8fbbce3c2854bd4bc9
Karl Hobley <karlhobley10@gmail.com>
2014-12-09 19:38:51 +0000

    Reorg of docker/local.py

--- docker/local.py
+++ docker/local.py
@@ -2,23 +2,28 @@ import os
 import dj_database_url
 
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite://{{ project_name }}.db')
-ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL', None)
-ELASTICSEARCH_INDEX = os.environ.get('ELASTICSEARCH_INDEX', '{{ project_name }}')
-REDIS_URL = os.environ.get('REDIS_URL', None)
-REDIS_KEY_PREFIX = os.environ.get('REDIS_KEY_PREFIX', '{{ project_name }}')
-
+# Basic settings
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') or []
 
+
+# Database
+
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite://{{ project_name }}.db')
 
 DATABASES = {
     'default': dj_database_url.parse(DATABASE_URL),
 }
 
 
+# Elasticsearch
+
+ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL', None)
+
 if ELASTICSEARCH_URL:
+    ELASTICSEARCH_INDEX = os.environ.get('ELASTICSEARCH_INDEX', '{{ project_name }}')
+
     WAGTAILSEARCH_BACKENDS = {
         'default': {
             'BACKEND': 'wagtail.wagtailsearch.backends.elasticsearch.ElasticSearch',
@@ -34,7 +39,13 @@ else:
     }
 
 
+# Redis
+
+REDIS_URL = os.environ.get('REDIS_URL', None)
+
 if REDIS_URL:
+    REDIS_KEY_PREFIX = os.environ.get('REDIS_KEY_PREFIX', '{{ project_name }}')
+
     CACHES = {
         'default': {
             'BACKEND': 'redis_cache.cache.RedisCache',
